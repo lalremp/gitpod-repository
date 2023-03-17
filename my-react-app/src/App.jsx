32643f6cbce4296f8d0cba2a9453cfa4cc40541c
lalremp piang <khinchana101@gmail.com>
2023-03-17 06:57:28 +0000

    Add some more changes!

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+import Message from './Message'
+
+function App() {
+  return(
+    <Message />
+  )
+}
+
+export default App;
---
+++ my-react-app/src/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Message() {
+    return(
+        <h1 className='container text-center'>Hello! This is for the Message</h1>
+    )
+}
+
+export default Message;
